021d00084612187e9ca7efdc95439f65a59d02ef
thatsmniti <134503971+thatsmniti@users.noreply.github.com>
2023-10-18 09:53:32 -0300

    Add files via upload

---
+++ SQLite.sql
@@ -0,0 +1,33 @@
+DROP TABLE demo;
+
+ 
+
+CREATE TABLE CLIENTE (
+
+ID_Cliente INT NOT NULL,
+
+Nome VARCHAR (1055) NOT NULL,
+
+CPF char(14) NOT NULL,
+
+RG char(12) NOT NULL,
+
+Logradouro varchar(100) NOT NULL,
+
+CEP char(9) NOT NULL,
+
+UF char(2) NOT NULL,
+
+Cidade varchar(30) NOT NULL,
+
+Telefone char(16) NOT NULL,
+
+Email varchar(355) NOT NULL,
+
+CNH char(9) NOT NULL,
+
+Renda float(10) Null
+
+ 
+
+);
